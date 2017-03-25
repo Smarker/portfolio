0fc7cf64ec14875bf898492b761164c3c56f3201
Stephanie Marker <Stephanie.Marker93@gmail.com>
2017-03-25 19:07:19 -0400

    style: edit about photos for mobile and adjust padding

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,10 +3,10 @@ import React from 'react';
 
 import './style.css';
 
-import profile from '../../../public/images/profile.jpg';
-import climb from '../../../public/images/iceland-climb.jpg';
-import hike from '../../../public/images/hiking.jpg';
-import ski from '../../../public/images/ski.jpg';
+import profile from '../../../public/images/profile-bw.jpg';
+import climb from '../../../public/images/iceland-climb-bw.jpg';
+import hike from '../../../public/images/hiking-bw.jpg';
+import ski from '../../../public/images/ski-bw.jpg';
 
 import { Container, Header, Item, Image} from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
@@ -21,7 +21,7 @@ export default class About extends React.Component {
             <Header as='h1' style={{'font-size': '3rem'}} inverted>About</Header>
             <Item.Group relaxed>
               <Item>
-                <Item.Image size='tiny' src={profile} className='bordered-image-white' size='small' shape='circular' />
+                <Item.Image src={profile} className='bordered-image-white' size='tiny' shape='circular' />
                 <Item.Content verticalAlign='middle'>
                   <p className="paragraph-white">
                     Stephanie is a software developer who enjoys solving complex problems from a mathematical viewpoint. She seeks out challenges and learning opportunities. When she has free time, she likes to work on <Link to="/projects" className="link-underline">personal projects</Link>.
@@ -41,10 +41,10 @@ export default class About extends React.Component {
         <div className="parallax"></div>
         <Container style={{'padding-top': '50px', 'padding-bottom': '50px', 'background-color': '#22252C'}} fluid>
           <Container text textAlign='center'>
-            <Image.Group size='small'>
-              <Image src={climb} className='bordered-image-white spaced-image' shape='circular' />
-              <Image src={hike} className='bordered-image-white spaced-image' shape='circular' />
-              <Image src={ski} className='bordered-image-white spaced-image' shape='circular' />
+            <Image.Group size='tiny'>
+              <Image src={climb} className='bordered-image-white' shape='circular' />
+              <Image src={hike} className='bordered-image-white' shape='circular' />
+              <Image src={ski} className='bordered-image-white' shape='circular' />
             </Image.Group>
             <p className="paragraph-white">When she is not coding, she likes playing volleyball, skiing, bouldering, and backpacking. Last September, she visited Iceland to go exploring and appreciate nature. Her favorite part of the trip was glacier hiking!</p>
           </Container>
