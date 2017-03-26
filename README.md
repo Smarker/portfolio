bec428daace8e676d1d9a0ccf16db1798662ffae
Stephanie Marker <Stephanie.Marker93@gmail.com>
2017-03-26 15:20:23 -0400

    docs: Edit readme

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+# Stephanie Marker's portfolio
 
-See in progress portfolio at www.stephaniemarker.com
+I am still working on my developer portfolio, but you can take a Look at what I have done so far [here](www.stephaniemarker.com).
+
+## Known Issues:
+
+* CSS transitions on nav do not display on browsers other than firefox.
+* About page images load too slowly
+
+## Next Enhancements
+
+* Create projects page
+* Write blog post
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import { Item } from 'semantic-ui-react';
+
+import slack from '../../../public/images/projects/slack-logo.svg';
+
 export default class Projects extends React.Component {
   constructor(props) {
     super(props);
@@ -8,7 +12,20 @@ export default class Projects extends React.Component {
   render() {
     return (
       <div className="content">
-        this is projects
+        <Item.Group>
+          <Item>
+            <Item.Image size='tiny' src={slack} />
+
+            <Item.Content>
+              <Item.Header>Arrowhead Valley Camp</Item.Header>
+              <Item.Meta>
+                <span className='price'>$1200</span>
+                <span className='stay'>1 Month</span>
+              </Item.Meta>
+              <Item.Description>yoooo</Item.Description>
+            </Item.Content>
+          </Item>
+        </Item.Group>
       </div>
     );
   }
