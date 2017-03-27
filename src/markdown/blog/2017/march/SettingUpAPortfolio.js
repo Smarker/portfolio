20dc96aca47099a8b3d6d00d65a0a407963997d6
Stephanie Marker <Stephanie.Marker93@gmail.com>
2017-03-26 21:10:24 -0400

    feat: add blog post markdown loader functionality

---
+++ src/markdown/blog/2017/march/SettingUpAPortfolio.js
@@ -0,0 +1,57 @@
+let SettingUpAPortfolio = `When creating a developer portfolio, it is good to first
+plan the structure of your site and determine the technologies needed to make it happen.
+
+## Planning the Structure of Your Portfolio
+
+I wanted to have a mobile-friendly site that would showcase my projects and skills,
+while also providing enough space for a tech blog. I found that looking at a lot of examples of others'
+developer portfolios helped me figure out how I wanted to lay out my site. I made sure to keep alert for
+elements I wanted to include on my site, even if it was just as I normally browsed websites.
+
+### My Portfolio's Structure:
+* Home + Tech Blog
+* About
+* Projects
+
+## Determining What Technologies to Use
+
+To determine the technologies needed, I considered the functionality I needed for the site. I chose Heroku
+for web hosting, because it was free and easy to use. I picked namecheap for my domain name, since it had
+cheaper domain names than GoDaddy. React would enable me to create my site quickly and easily. I liked it
+that React makes you split pieces of functionality into re-usable components. Semantic UI for React enabled
+me to create a site friendly for computers, tablets, and mobile devices. I wanted the site to be able to contain
+a collection of blog posts with user comments. Down the line, I would need to store user comments in a database.
+
+### How I Set Up My Site:
+
+#### Front End
+
+* Semantic UI React
+* React
+
+#### Back End
+* Node
+* Express
+* MongoDB
+
+#### Web Hosting
+* Heroku
+
+#### Domain
+* Namecheap
+
+## Improving the Usability and Appearance of Your Dev Portfolio
+You can make sure that your site is mobile friendly by checking it with Google's
+[mobile friendly test](https://search.google.com/search-console/mobile-friendly).
+
+## Keeping Track of Site Visitors
+You can keep track of the number of visitors to your site, visitor demographics, number of active users,
+etc. with [Google Analytics](https://analytics.google.com/).
+
+## Improving the Page Rank Your Portfolio
+As I was setting up my site's pages and content, I realized that my portfolio did not show up in searches.
+I wanted to make my site known, so I improved the searchability of my site by doing the following:
+
+(POST STILL IN PROGRESS)`;
+
+export default SettingUpAPortfolio;
