69846b6b487b9da93d8897eb2658825ae5a84092
Stephanie Marker <Stephanie.Marker93@gmail.com>
2017-04-02 21:39:28 -0400

    style:fix mobile

--- src/components/Blog/Post.js
+++ src/components/Blog/Post.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './style.css';
 
-import { Container, Header, Image, Button, Grid } from 'semantic-ui-react'
+import { Container, Header, Image, Button } from 'semantic-ui-react'
 
 import ReactMarkdown from 'react-markdown';
 import { Link } from 'react-router-dom'
@@ -13,80 +13,30 @@ let Post = (props) => {
     {/* Post Preview */}
     if(props.link !== "") {
       return (
-        <Grid centered columns={1}>
-          <Grid.Column className="mobile only" rows={2} >
-            <Grid.Row>
-              <Link to={props.link}>
-                <Image src={props.image} className="image-style" fluid />
-              </Link>
-            </Grid.Row>
-            <Grid.Row>
-                <Link to={props.link}>
-                  <Header as='h1' className="pink-header-mobile">
-                    <Header.Content>
-                      {props.title}
-                      <Header sub className="purple-subheader">
-                        {props.created}
-                      </Header>
-                    </Header.Content>
-                  </Header>
-                </Link>
-                <ReactMarkdown source={props.markdown} className="blog-style" />
-                <Link to={props.link}>
-                  <Button floated='right' inverted className="see-more-button">
-                    See More
-                  </Button>
-                </Link>
-            </Grid.Row>
-          </Grid.Column>
-          <Grid.Column className="computer only" style={{'margin-top':'75px'}}>
-            <Container text>
-              <Link to={props.link}>
-                <Image src={props.image} size='huge' className="image-style" />
-                <Header as='h1' className="pink-header">
-                  <Header.Content>
-                    {props.title}
-                    <Header sub className="purple-subheader">
-                      {props.created}
-                    </Header>
-                  </Header.Content>
+        <Container text className="post-padding-preview" >
+          <Link to={props.link}>
+            <Image src={props.image} size='huge' className="image-style" />
+            <Header as='h1' className="pink-header">
+              <Header.Content>
+                {props.title}
+                <Header sub className="purple-subheader">
+                  {props.created}
                 </Header>
-              </Link>
-              <ReactMarkdown source={props.markdown} className="blog-style" />
-              <Link to={props.link}>
-                <Button floated='right' inverted className="see-more-button">
-                  See More
-                </Button>
-              </Link>
-            </Container>
-          </Grid.Column>
-          <Grid.Column className="tablet only" style={{'margin-top':'75px'}}>
-            <Container text>
-              <Link to={props.link}>
-                <Image src={props.image} size='huge' className="image-style" />
-                <Header as='h1' className="pink-header">
-                  <Header.Content>
-                    {props.title}
-                    <Header sub className="purple-subheader">
-                      {props.created}
-                    </Header>
-                  </Header.Content>
-                </Header>
-              </Link>
-              <ReactMarkdown source={props.markdown} className="blog-style" />
-              <Link to={props.link}>
-                <Button floated='right' inverted className="see-more-button">
-                  See More
-                </Button>
-              </Link>
-            </Container>
-          </Grid.Column>
-        </Grid>
+              </Header.Content>
+            </Header>
+          </Link>
+          <ReactMarkdown source={props.markdown} className="blog-style" />
+          <Link to={props.link}>
+            <Button floated='right' inverted className="see-more-button">
+              See More
+            </Button>
+          </Link>
+        </Container>
       );
     {/* Full Post */}
     } else {
       return (
-        <div>
+        <div className="post-padding-full">
           <Image src={props.image} className="image-style" fluid />
           <Container text>
               <Header as='h1' className="pink-header">
