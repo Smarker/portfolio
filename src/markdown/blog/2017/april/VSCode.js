f91507af176701a1b4559c13f38b61ff6b264d05
Stephanie Marker <Stephanie.Marker93@gmail.com>
2017-04-22 12:43:33 -0400

    refactor: Add content to vscode post

--- src/markdown/blog/2017/april/VSCode.js
+++ src/markdown/blog/2017/april/VSCode.js
@@ -6,7 +6,43 @@ import { Image } from 'semantic-ui-react'
 const preview = `this is where ill talk about vs
 `;
 
-const vscode = `this is where ill talk about vs
+const vscode = `##About VS Code
+
+Visual Studio Code is a free open source IDE that comes with a bunch of features like autocompletion,
+integration with git, debugging, and extensions. I have been using this IDE for a while now after realizing that it is much
+less laggy than Atom is when editing code. I also like how quickly I can make commits to git right through the editor. Also,
+it comes with the ability to download customized extensions like 'Debugger for Chrome' and 'Reactjs code snippets' which 
+really speed up development.
+
+## Adding Extensions
+
+To add an extension like 'Debugger for Chrome' follow these steps:
+
+* Search for the extension in the marketplace within vs code
+* Install the extension
+* Within the .vscode folder, open up launch.json
+* Edit launch.json and add a configuration. You wil need to specify whether you want to run the debugger against a file or url. 
+See [here](https://github.com/Microsoft/vscode-chrome-debug) for more information on what to put in the launch.json file.
+
+For example, this is what I used in my launch.json file:
+
+\`\`\`
+{
+  "version": "0.2.0",
+  "configurations": [{
+    "name": "Chrome",
+    "type": "chrome",
+    "request": "launch",
+    "url": "http://localhost:3000",
+    "webRoot": "\${workspaceRoot}/src",
+    "userDataDir": "\${workspaceRoot}/.vscode/chrome",
+    "sourceMapPathOverrides": {
+      "webpack:///src/*": "\${webRoot}/*"
+    }
+  }]
+}
+\`\`\`
+
 `;
 
 const VSCodePreview = <ReactMarkdown source={preview} className="blog-style" />
@@ -17,7 +53,7 @@ let VSCodePreviewPost = () => (
     <Post 
         link="/vs-code"
         image="../../../images/blog/create-portfolio.jpg" 
-        title="Why my Preferred IDE is Visual Studio Code" 
+        title="Adding Extensions Visual Studio Code" 
         created="April 19, 2017" 
         content={ VSCodePreview }
     />
@@ -27,7 +63,7 @@ let VSCodePost = () => (
     <Post 
         link=""
         image="../../../images/blog/create-portfolio.jpg" 
-        title="Why my Preferred IDE is Visual Studio Code" 
+        title="Adding Extensions Visual Studio Code" 
         created="April 19, 2017" 
         content={ VSCode }
     />
