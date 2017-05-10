6b34217bd31212a95d5d6aa0863d965ebbdaba67
Stephanie Marker <Stephanie.Marker93@gmail.com>
2017-05-09 21:11:30 -0400

    small changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,3 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from "./index.js";
-
-it('renders without crashing', () => {
-  ReactDOM.render(<Root />,
-  document.getElementById('root'));
-});
